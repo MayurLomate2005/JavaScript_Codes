359023a7c0be2356ab0440809631c26f0d45a773
Mayur Lomate <mayurlomate2005@gmail.com>
2025-05-14 18:12:26 +0000

    datatypes

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,7 @@
- 
+const accountId = 1233
+let accountName = " Mayur Lomate"
+var accountEmail = "mayurlomate2005@gmail.com"
+let pass = "12234"
+
+console.table([accountId,accountName,accountEmail,pass]);
+// prefer not use var instead of use let , const
