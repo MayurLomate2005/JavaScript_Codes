f90c99464fa13e96f441d9dd0336f409a63c08e4
Mayur Lomate <mayurlomate2005@gmail.com>
2025-05-14 18:57:03 +0000

    TypesofData

--- Basics/test.js
+++ Basics/test.js
@@ -14,3 +14,31 @@ let conversion = Number(value);
 console.log(conversion) // its 45 means it convert string into number but if we try for undefined it is undefined and for null its Nan(Not a Number)
 // important Note : When You try for conversion of a one datatype into another check the value sometimes it gives NAN , which can lead to confusion 
 //further . 
+
+
+/*
+in javscript the DataTypes Used : Primitive and non primitive
+primitive : Number , String , Boolean , Null , Undefined and Symbol 
+Non-Primitive : Array , Object , Function 
+*/
+
+// Syntax of it : 
+let Array = ["Mayur" , "Babasaheb ","Lomate"]; // This is Array 
+
+let Function = function(){
+
+    console.log("Good Morning Guys");
+    
+}
+
+let obj = {
+    name : " mayur ",
+    age :  20,
+}
+
+console.table([Array , obj]);
+Function();
+
+
+// This is How the Reference Datatype are worked .
+
